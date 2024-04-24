50d6d7ee5cd5ac93cb7fed60668b60886da76773
Jinalivish <jinali.vish@gmail.com>
2024-04-23 21:02:52 -0400

    Resolved bugs and set logout functinality and delete post functionality

---
+++ EventHandlersDemo/MyEventArgs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class MyEventArgs : EventArgs
+{
+    public string Message { get; }
+
+    public MyEventArgs(string message)
+    {
+        Message = message;
+    }
+}
+
+
+public delegate void MyEventHandler(object sender, MyEventArgs e);
+
+public class EventPublisher
+{
+    public event MyEventHandler MyEvent;
+
+    public void RaiseEvent(string message)
+    {
+        OnMyEvent(new MyEventArgs(message));
+    }
+
+    protected virtual void OnMyEvent(MyEventArgs e)
+    {
+        MyEvent?.Invoke(this, e);
+    }
+}
+
+public class EventHandler
+{
+    public void HandleEvent(object sender, MyEventArgs e)
+    {
+        Console.WriteLine($"Event handled: {e.Message}");
+    }
+}
